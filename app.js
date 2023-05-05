3d12185bd85c4db5023bb2f257039ff62cfcf8ec
0xNim <57798165+Nim1com@users.noreply.github.com>
2023-05-05 10:08:16 -0400

    added stuff

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const mysql = require('mysql2/promise');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +13,8 @@ const cors = require('cors');
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Health Check
 
@@ -74,6 +78,119 @@ app.get('/mods/:modID', async (req, res) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 });
+
+//Get all versions of a specific mod:
+//Method: GET
+//URL: /mods/{modID}/versions
+//Example: /mods/UITools/versions
+app.get('/mods/:modID/versions', async (req, res) => {
+  try {
+    const { modID } = req.params;
+
+    const [versions, fields2] = await pool.query('SELECT * FROM ModVersions WHERE modID = ?', [modID]);
+
+    if (versions.length === 0) {
+      return res.status(404).json({ message: 'Mod or versions not found' });
+    }
+
+    return res.json(versions);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+//Get a specific version of a mod:
+//Method: GET
+//URL: /mods/{modID}/versions/{versionNumber}
+//Example: /mods/UITools/versions/1.0
+
+app.get('/mods/:modID/versions/:versionNumber', async (req, res) => {
+  try {
+    const { modID, versionNumber } = req.params;
+
+    const [versions, fields2] = await pool.query('SELECT * FROM ModVersions WHERE modID = ? AND versionNumber = ?', [modID, versionNumber]);
+
+    if (versions.length === 0) {
+      return res.status(404).json({ message: 'Mod or version not found' });
+    }
+
+    return res.json(versions[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Get all files for a specific mod version:
+//Method: GET
+//URL: /versions/{versionId}/files
+//Example: /versions/11/files
+app.get('/versions/:versionID/files', async (req, res) => {
+  try {
+    const { versionID } = req.params;
+
+    const [files, fields2] = await pool.query('SELECT * FROM ModFiles WHERE modVersionID = ?', [versionID]);
+    
+    if (files.length === 0) {
+      return res.status(404).json({ message: 'Mod, version, or files not found' });
+    }
+
+    return res.json(files);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+// Get a specific file for a specific mod version:
+//Method: GET
+//URL: /versions/{versionId}/files/{fileID}
+//Example: /versions/11/files/1
+app.get('/versions/:versionID/files/:fileID', async (req, res) => {
+  try {
+    const { versionID, fileID } = req.params;
+
+    const [files, fields2] = await pool.query('SELECT * FROM ModFiles WHERE modVersionID = ? AND fileID = ?', [versionID, fileID]);
+
+    if (files.length === 0) {
+      return res.status(404).json({ message: 'Mod, version, or file not found' });
+    }
+
+    return res.json(files[0]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+//Get all dependencies for a specific mod version:
+//Method: GET
+//URL: /versions/{versionId}/dependencies
+//Example: /versions/11/dependencies
+
+app.get('/versions/:versionId/dependencies', async (req, res) => {
+  try {
+    const { versionId } = req.params;
+
+    const [dependencies, fields2] = await pool.query('SELECT * FROM ModDependencies WHERE modVersionId = ?', [versionId]);
+
+    if (dependencies.length === 0) {
+      return res.status(404).json({ message: 'Mod, version, or dependencies not found' });
+    }
+
+    return res.json(dependencies);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+
+
+
+
 
 
 // Export the app for testing
--- test/mods.test.js
+++ test/mods.test.js
@@ -13,37 +13,215 @@ describe('GET /mods', () => {
 
   
 describe('GET /mods/:modID', () => {
-    test('should return the correct mod', async () => {
-      // Make a request to the endpoint
-      const res = await request(app).get('/mods/modIDTEST');
-      
-      // Check that the response has a 200 status code
-      expect(res.statusCode).toBe(200);
-      
-      // Check that the response body contains the correct mod
-      expect(res.body).toEqual({
-        modID: 'modIDTEST',
-        modIcon: null,
-        modName: 'ModID Test',
-        modAuthor: 'nim',
-        modDescription: 'This is not a mod and it is used for API tests.',
-        modVersion: '0.0.1',
-        modReleaseDate: '2023-05-04T04:00:00.000Z',
-        modTags: 'test,official'
-      });
-    });
-  
-    test('should return a 404 if the mod is not found', async () => {
-      // Make a request to the endpoint with an invalid mod ID
-      const res = await request(app).get('/mods/invalidModIDTEST');
-      
-      // Check that the response has a 404 status code
-      expect(res.statusCode).toBe(404);
-      
-      // Check that the response body contains the correct message
-      expect(res.body).toEqual({
-        message: 'Mod not found'
-      });
+  test('should return the correct mod', async () => {
+    // Make a request to the endpoint
+    const res = await request(app).get('/mods/modIDTEST');
+    
+    // Check that the response has a 200 status code
+    expect(res.statusCode).toBe(200);
+    
+    // Check that the response body contains the correct mod
+    expect(res.body).toEqual({
+      modID: 'modIDTEST',
+      modIcon: null,
+      modName: 'ModID Test',
+      modAuthor: 'nim',
+      modDescription: 'This is not a mod and it is used for API tests.',
+      modVersion: '0.0.1',
+      modReleaseDate: '2023-05-04T04:00:00.000Z',
+      modTags: 'test,official'
     });
   });
+
+  test('should return a 404 if the mod is not found', async () => {
+    // Make a request to the endpoint with an invalid mod ID
+    const res = await request(app).get('/mods/invalidModIDTEST');
+    
+    // Check that the response has a 404 status code
+    expect(res.statusCode).toBe(404);
+    
+    // Check that the response body contains the correct message
+    expect(res.body).toEqual({
+      message: 'Mod not found'
+    });
+  });
+});
+
+describe('GET /mods/:modID/versions', () => {
+  test('should return the correct versions', async () => {
+    // Make a request to the endpoint
+    const res = await request(app).get('/mods/modIDTEST/versions');
+    
+    // Check that the response has a 200 status code
+    expect(res.statusCode).toBe(200);
+    
+    // Check that the response body contains the correct versions
+    expect(res.body).toEqual([
+      {
+        modVersionID: 11,
+        modID: 'modIDTEST',
+        versionNumber: '1.0.1',
+        releaseDate: '2023-05-05T04:00:00.000Z',
+        changelog: 'Added nothing this is a test version.'
+      }
+    ]);
+  });
+
+  test('should return a 404 if the mod is not found', async () => {
+    // Make a request to the endpoint with an invalid mod ID
+    const res = await request(app).get('/mods/invalidModIDTEST/versions');
+    
+    // Check that the response has a 404 status code
+    expect(res.statusCode).toBe(404);
+    
+    // Check that the response body contains the correct message
+    expect(res.body).toEqual({
+      message: 'Mod or versions not found'
+    });
+  });
+}
+);
+
+describe('GET /mods/:modID/versions/:versionNumber', () => {
+  test('should return the correct version', async () => {
+    // Make a request to the endpoint
+    const res = await request(app).get('/mods/modIDTEST/versions/1.0.1');
+    
+    // Check that the response has a 200 status code
+    expect(res.statusCode).toBe(200);
+    
+    // Check that the response body contains the correct version
+    expect(res.body).toEqual({
+      modVersionID: 11,
+      modID: 'modIDTEST',
+      versionNumber: '1.0.1',
+      releaseDate: '2023-05-05T04:00:00.000Z',
+      changelog: 'Added nothing this is a test version.'
+    });
+  });
+
+  test('should return a 404 if the version is not found', async () => {
+    // Make a request to the endpoint with an invalid version number
+    const res = await request(app).get('/mods/modIDTEST/versions/invalidVersionNumber');
+    
+    // Check that the response has a 404 status code
+    expect(res.statusCode).toBe(404);
+    
+    // Check that the response body contains the correct message
+    expect(res.body).toEqual({
+      message: 'Mod or version not found'
+    });
+  });
+});
+
+
+describe('GET /versions/:versionID/files', () => {
+  test('should return the correct files', async () => {
+    // Make a request to the endpoint
+    const res = await request(app).get('/versions/11/files');
+    
+    // Check that the response has a 200 status code
+    expect(res.statusCode).toBe(200);
+    
+    // Check that the response body contains the correct files
+    expect(res.body).toEqual([
+      {
+        fileID: 8,
+        modVersionID: "11",
+        fileType: "mod",
+        fileSize: 1,
+        fileURL: "cdn.astromods.xyz/test",
+        uploadDate: null
+      }
+    ]);
+  });
+  
+  test('should return a 404 if the version is not found', async () => {
+    // Make a request to the endpoint with an invalid version ID
+    const res = await request(app).get('/versions/invalidVersionID/files');
+    
+    // Check that the response has a 404 status code
+    expect(res.statusCode).toBe(404);
+    
+    // Check that the response body contains the correct message
+    expect(res.body).toEqual({
+      message: 'Mod, version, or files not found'
+    });
+  });
+});
+
+describe('GET /versions/:versionID/files/:fileID', () => {
+  test('should return the correct file', async () => {
+    // Make a request to the endpoint
+    const res = await request(app).get('/versions/11/files/8');
+    
+    // Check that the response has a 200 status code
+    expect(res.statusCode).toBe(200);
+    
+    // Check that the response body contains the correct file
+    expect(res.body).toEqual({
+      fileID: 8,
+      modVersionID: "11",
+      fileType: "mod",
+      fileSize: 1,
+      fileURL: "cdn.astromods.xyz/test",
+      uploadDate: null
+    });
+  }
+  );
+
+  test('should return a 404 if the file is not found', async () => {
+    // Make a request to the endpoint with an invalid file ID
+    const res = await request(app).get('/versions/11/files/invalidFileID');
+    
+    // Check that the response has a 404 status code
+    expect(res.statusCode).toBe(404);
+    
+    // Check that the response body contains the correct message
+    expect(res.body).toEqual({
+      message: 'Mod, version, or file not found'
+    });
+  }
+  );
+}
+);
+
+
+describe('GET /versions/:versionID/dependencies', () => {
+  test('should return the correct dependencies', async () => {
+    // Make a request to the endpoint
+    const res = await request(app).get('/versions/11/dependencies');
+    
+    // Check that the response has a 200 status code
+    expect(res.statusCode).toBe(200);
+    
+    // Check that the response body contains the correct dependencies
+    expect(res.body).toEqual([
+      {
+        id: 4,
+        modVersionId: "11",
+        dependencyModID: "UITools",
+        minimumDependencyVersion: "1.0.0",
+        maximumDependencyVersion: "1.0.1",
+        dependencyType: "optional"
+      }
+    ]);
+  }
+  );
+
+  test('should return a 404 if the version is not found', async () => {
+    // Make a request to the endpoint with an invalid version ID
+    const res = await request(app).get('/versions/invalidVersionID/dependencies');
+    
+    // Check that the response has a 404 status code
+    expect(res.statusCode).toBe(404);
+    
+    // Check that the response body contains the correct message
+    expect(res.body).toEqual({
+      message: 'Mod, version, or dependencies not found'
+    });
+  }
+  );
+});
+
   
